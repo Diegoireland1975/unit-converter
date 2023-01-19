97c7334ee3391696cf706fd7642afebb51ba7516
Diego Raffa <diego170975@gmail.com>
2023-01-19 13:21:54 +0000

    added meter to mile conversion

--- js/main.js
+++ js/main.js
@@ -4,19 +4,25 @@
 1 kilogram = 2.204 pound
 */
 const meterToFeet = 3.281;
+const meterToMile = 0.00062;
 const literToGallon = 0.264;
 const kiloToPound = 2.204;
 const input = document.getElementById("input");
 const convertBtn = document.getElementById("convert-btn");
+const heigthEl = document.getElementById("heigth-el");
 const lengthEl = document.getElementById("length-el");
 const volumeEl = document.getElementById("volume-el");
 const massEl = document.getElementById("mass-el");
 
 convertBtn.addEventListener("click", () => {
   let baseValue = input.value;
-  lengthEl.textContent = `${baseValue} meter = ${(
+  heigthEl.textContent = `${baseValue} meter = ${(
     baseValue * meterToFeet
   ).toFixed(3)} feet`;
+  lengthEl.textContent = `${baseValue} meter = ${(
+    baseValue * meterToMile
+  ).toFixed(3)} mile`;
+
   volumeEl.textContent = `${baseValue} liter = ${(
     baseValue * literToGallon
   ).toFixed(3)} feet`;
